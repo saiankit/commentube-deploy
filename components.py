4b3c705ad5bf98466e2c5493d91c3ec6f05f178b
Sai Ankit <saiankit30@gmail.com>
2023-06-12 20:11:01 +0530

    demo

--- components.py
+++ components.py
@@ -1,30 +1,36 @@
 def card(text, label, sentiment):
     if sentiment == "Positive":
         return f"""
-        <div class="card bg-dark m-4" style="width: 18rem;">
+        <div class="card bg-dark m-4" style="width: 30rem;">
             <div class="card-body">
+                <div class="flex flex-row justify-content-between">
                     <h5 class="lead">{label}</h5>
-                <span class="badge badge-success">Postive</span>
+                    <span class="badge badge-success">Postive</span>
+                <div>
                 <p class="card-text">{text}</p>
             </div>
         </div>
         """
     elif sentiment == "Negative":
         return f"""
-        <div class="card bg-dark m-4" style="width: 18rem;">
+        <div class="card bg-dark m-4" style="width: 30rem;">
             <div class="card-body">
-                <h5 class="lead">{label}</h5>
-                <span class="badge badge-danger">Negative</span>
+                <div class="flex flex-row justify-content-between">
+                    <h5 class="lead">{label}</h5>
+                    <span class="badge badge-danger">Negative</span>
+                </div>
                 <p class="card-text">{text}</p>
             </div>
         </div>
         """
     else:
         return f"""
-        <div class="card bg-dark m-4" style="width: 18rem;">
+        <div class="card bg-dark m-4" style="width: 30rem;">
             <div class="card-body">
-                <h5 class="lead">{label}</h5>
-                <span class="badge badge-warning">Neutral</span>
+                    <div class="flex flex-row justify-content-between">
+                        <h5 class="lead">{label}</h5>
+                        <span class="badge badge-warning">Neutral</span>
+                    </div>
                 <p class="card-text">{text}</p>
             </div>
         </div>
