f8ae8b322aae7a9bd846a1494023c97b190334ec
Sai Ankit <saiankit30@gmail.com>
2023-06-28 15:14:57 +0530

    demo

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-import streamlit as st 
+import streamlit as st
 import streamlit.components.v1 as components
-from preprocess import pre_process 
-from components import card
-from utilities import generate_text , generate_image_link
 import time
-import pandas as pd
-from youtube_comment_downloader import *
 import numpy as np
+import pandas as pd
 import torch
 from sklearn.metrics.pairwise import cosine_similarity
-from sentence_transformers import SentenceTransformer, util
-from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config, pipeline
 from sklearn.cluster import AgglomerativeClustering
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import nltk
 from nltk.tokenize import sent_tokenize
-from sentiment_analysis import sentiment_analyse
 import ktrain
 from ktrain.text.sentiment import SentimentAnalyzer
+from preprocess import pre_process
+from components import card
+from utilities import generate_text, generate_image_link
+from youtube_comment_downloader import *
+from sentence_transformers import SentenceTransformer, util
+from transformers import (
+    T5Tokenizer, T5ForConditionalGeneration, T5Config, pipeline,
+    AutoModelForSequenceClassification, AutoTokenizer
+)
+from sentiment_analysis import sentiment_analyse
+from streamlit_option_menu import option_menu
+
+pd.set_option('mode.chained_assignment', None)  # Disable pandas SettingWithCopyWarning
 
 st.set_page_config(
-    page_title="CommentTube",
-    page_icon="💬",
+    page_title="CommenTube",
+    page_icon="📽️",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -35,6 +40,17 @@ st.markdown(
      unsafe_allow_html=True
 )
 
+with st.sidebar:
+    choose = option_menu("CommenTube📽️", ["About", "Classification", "Statistics", "Clustering"],
+                         icons=['house', 'camera fill', 'kanban', 'book'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#0F1116"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
 def embedd_sentences(sentences, model_name):
     """
@@ -94,8 +110,20 @@ def cluster_sentences(sentences, sentence_embedding, distance_threshold):
 #predictor = ktrain.load_predictor('model1')
 #classifier = SentimentAnalyzer()
 
+def downloadCommentsFromURL(youtube_url):
+    downloader = YoutubeCommentDownloader()
+    comments = downloader.get_comments_from_url(youtube_url)
+    comments_data = []
+    for comment in comments:
+        data = {}
+        data["text"] = comment["text"]
+        data["cid"] = comment["cid"]
+        data["time_parsed"] = comment["time"]
+    json_data = json.dumps(comments_data)
+    df = pd.read_json(json_data)
+
+
 def downloadComments():
-    st1 = time.process_time()    
     downloader = YoutubeCommentDownloader()
     comments = downloader.get_comments_from_url(youtube_url)
     comments_data = []
@@ -106,9 +134,6 @@ def downloadComments():
         comments_data.append(data)
     json_data = json.dumps(comments_data)
     df = pd.read_json(json_data)
-    et = time.process_time()
-    res = et - st1
-    print('Total time:', res, 'seconds')
     sentences, original = pre_process(df)
     #out = predictor.predict(sentences)
    # sentiments = classifier.predict(sentences)
@@ -155,8 +180,26 @@ def process_sentiments(sentiments):
             sentiments_final.append("Negative")
     return sentiments_final
 
+def showCards(originals, sentiments, predictions):
+    labels = st.session_state['labels']
+    col11, col22 = st.columns(2)
+    print(labels)
+    for i in range(0, len(originals)):
+        if (i % 2 == 0 ):
+            col11.write(card(originals[i], predictions[i], sentiments[i]), unsafe_allow_html=True)
+        else:
+            col22.write(card(originals[i], predictions[i], sentiments[i]), unsafe_allow_html=True)
 
-col1.header("CommenTube📽️")
+
+if 'labels' not in st.session_state:
+    st.session_state['labels'] =  ['interrogative', 'imperative', 'corrective', 'miscellaneous', 'others']
+
+def handle_click(selected_labels):
+    st.session_state['labels'] = selected_labels
+
+comment_labels = ['interrogative', 'imperative', 'corrective', 'miscellaneous', 'others']
+selected_labels = []
+
 youtube_url = col1.text_input('Enter Youtube URL')
 col1.write('(or)')
 selected = col1.selectbox('Select from Example videos', ('','Example 1', 'Example 2'))
@@ -171,18 +214,15 @@ if col1.button('Run 🪄'):
             originals = df['original'].to_list()
             sentiments = process_sentiments(df['sentiments'].to_list())
             predictions = df['predictions'].to_list()
-            # options = st.multiselect(
-            # 'Choose Labels',
-            # ['interrogative', 'imperative', 'corrective', 'miscellaneous', 'others'],
-            # ['interrogative', 'imperative', 'corrective', 'miscellaneous', 'others'],
-            # )
+            st.write(st.session_state)
+            selected_labels = st.multiselect(
+                'Choose Labels',
+                options=comment_labels,
+                default=st.session_state.labels,
+                on_change=handle_click,
+                args=(selected_labels,)
+            )
+            st.session_state['labels'] = selected_labels 
 
-            col11, col22 = st.columns(2)
-            for i in range(0, len(originals)):
-                if (i % 2 == 0 ):
-                    with col11:
-                        st.write(card(originals[i], predictions[i], sentiments[i]), unsafe_allow_html=True)
-                else:
-                    with col22:
-                        st.write(card(originals[i], predictions[i], sentiments[i]), unsafe_allow_html=True)
-
+            showCards(originals, sentiments, predictions)
+            
